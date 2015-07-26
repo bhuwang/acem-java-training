d0e6506b171be4c08a09acc4190732f3edf7c1bb
Anup Khanal <cylonseverywhere@gmail.com>
2015-07-26 16:46:25 +0545

    #1-implemented display salutation program

--- src/com/bhuwan/java/training/Anup/HelloWorldDemo.java
+++ src/com/bhuwan/java/training/Anup/HelloWorldDemo.java
@@ -1,5 +1,22 @@
+
+
 package com.bhuwan.java.training.Anup;
+
+import java.util.Scanner;
+/* Write a hello world program which takes two argument 
+a. Salutation 
+b. Full Name and 
+c. Display Salutation Full Name in console.*/
 
 public class HelloWorldDemo {
 
+	 public static void main(String[] args){
+		System.out.println("Enter your Salutation");
+		Scanner scanner= new Scanner(System.in);
+		String salu= scanner.nextLine();
+		System.out.println("Enter your name");
+		String name= scanner.nextLine();
+		System.out.println(salu + name);
+		scanner.close();
+	 }
 }

d5592c3252882666eb663cc4a45322a084b80421
kabi <raikabindra29@gamil.com>
2015-07-26 16:48:15 +0545

    #1-completed implemented

---
+++ src/com/bhuwan/java/training/kabindra/HelloWorld.java
@@ -0,0 +1,30 @@
+package com.bhuwan.java.training.kabindra;
+import java.util.Scanner;
+/**
+ * <pre>
+ * Write a hello world program which takes two argument 
+a. Salutation 
+b. Full Name and 
+c. Display SalutationFull Name in console.
+*</pre>
+ * @author kabi
+ *
+ */
+public class HelloWorld {
+	/**
+	 * 
+	 */
+	public static void main(String[] args){
+		String s,n;
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter Salutation");
+		s=input.next();
+		System.out.print("Enter name");
+		n= input.next();
+		System.out.println(s+""+n);
+		input.close();
+		
+		
+	}
+
+}

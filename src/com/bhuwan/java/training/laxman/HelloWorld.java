f2a27d0fcf2245688ef8388a832b67852243b92d
laxman56 <lkhanal56@gmail.com>
2015-07-26 16:34:35 +0545

    #1 implemented display salutation program

---
+++ src/com/bhuwan/java/training/laxman/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.bhuwan.java.training.laxman;
+
+/**
+ * Created by laxman on 7/26/15.
+ */
+which takes two argument
+        a. Salutaion
+        b. Full Name and
+        c. Display SalutationFull Name in console.
+
+
+public class HelloWorld {
+    public void testing("hello world");
+}

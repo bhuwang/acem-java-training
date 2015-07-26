eb4feab7d0f7532998b803cfa80895fc5ae3ca7f
Himancu <dead_death96@yahoo.com>
2015-07-26 16:54:06 +0545

    1# implemented

---
+++ src/com/bhuwan/java/training/himancu/HelloWorldDemo.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.bhuwan.java.training.himancu;
+
+/**
+ * <pre>
+ * Write a hello world program which takes two argument 
+ * a. Salutation 
+ * b. Full Name and 
+ * c. Display Salutation Full Name in console.
+ * </pre>
+ * 
+ *@author Himancu<dead_death96@yahoo.com>
+ */
+public class HelloWorldDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char name ;
+		char salu_name;
+		
+
+	}
+
+}

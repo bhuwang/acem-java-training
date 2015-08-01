7a422297dbb9fa91c128ee8ea000fadccf72e023
Madan Dhungana <madandhungana@gmail.com>
2015-08-01 21:29:39 +0545

    #2 Created Temperature.java class that display celsius from 0 to 100 and converted and displayed fahrenheit

---
+++ src/com/bhuwan/java/training/madan/Temperature.java
@@ -0,0 +1,40 @@
+package com.bhuwan.java.training.madan;
+
+/**
+ * <pre>
+ *Temperature.java
+ *
+ *program that displays the temperatures from 0 degrees Celsius
+ * to 100 degrees Celsius and its Fahrenheit equivalent
+ *conversion from Celsius to Fahrenheit:-  F = C * 9/5 + 32
+ *
+ *Aug 1, 2015 8:57:07 PM
+ * @author madandhungana@gmail.com
+ *
+ *<pre>
+ */
+public class Temperature {
+	
+	public static void main(String []args){
+	
+		int i;
+		float fahrenheit;
+		final String DEG="\u2103";
+		final String FAHR="\u2109";
+		
+		System.out.println("------------------------------------");
+		System.out.println("Celsius \t\t Fahrenheit ");
+		System.out.println("------------------------------------");
+		
+		for(i=0;i<=100;i++){
+			
+			fahrenheit=(float)i*9/5+32;
+			System.out.println("   "+i+"\t\t\t    "+fahrenheit);
+			
+			
+		}
+		
+	}
+	
+	
+}

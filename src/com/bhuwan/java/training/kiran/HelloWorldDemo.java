0b3b76c3c04f08bc3cf6c6d1f822af36e7e6517b
kiranpariyar <kirannpariyar@gmail.com>
2015-08-05 21:38:47 +0545

    simple Collection practice

--- src/com/bhuwan/java/training/kiran/HelloWorldDemo.java
+++ src/com/bhuwan/java/training/kiran/HelloWorldDemo.java
@@ -3,6 +3,12 @@
  */
 package com.bhuwan.java.training.kiran;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 /**
  *<pre>
  * Write a hello world program which takes two argument 
@@ -25,8 +31,55 @@ public class HelloWorldDemo {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		getSalutation("Welcome","Kiran Pariyar");
-		System.out.println(args[0]+" "+args[1]);
-	}
+//		getSalutation("Welcome","Kiran Pariyar");
+//		System.out.println(args[0]+" "+args[1]);
+		/*
+		String name = "Kiran Pariyar";
+		name.concat("Kathmandu");
+		System.out.println(name);
+		System.out.println(name.concat("Kathmandu") + " :pass 2");
+		
+		StringBuffer name2 = new StringBuffer("Kiran Pariyar");
+		name2.append("Kathmandu");
+		System.out.println(name2);
+		
+		StringBuilder name3 = new StringBuilder("Hello World");
+		name3.append("Hi");
+		System.out.println(name3);
+		
+		*/
+		
+		List<String> computerList = new ArrayList<>();
+		computerList.add("Laxman");
+		computerList.add("Bishal");
+		
+		computerList.add("Laxman");
+		computerList.add("Madan");
+		computerList.add("Manish");
+		
+		computerList.add("Madan");
+		computerList.add("Manish");
+		computerList.remove("Manish");
+		
+		for(String stname : computerList){
+			System.out.println(stname);		
+		}
+			
+		Set<String> completeList = new HashSet<>(computerList);
+		
+//		System.out.println(completeList.size());
 
+		Iterator<String> iterator = completeList.iterator();
+		
+		iterator.remove();
+		
+		/*while(iterator.hasNext()){
+			System.out.println();
+		}*/
+		
+	/*	for(String stname : completeList){
+			System.out.println(stname);		
+		}*/
+	
+	}
 }
